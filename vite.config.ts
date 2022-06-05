b14283c55f8fab36481ba0486f49b8948bc4bd8d
John Rush <john@fluent-forever.com>
2022-06-05 01:53:07 -0700

    source maps for dev only

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,23 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command, mode }) => ({
   build: {
-    sourcemap: true,
-    assetsDir: "code",
-    chunkSizeWarningLimit: 1000
+    sourcemap: mode === 'development',
+    assetsDir: 'code',
+    chunkSizeWarningLimit: 1000,
   },
   plugins: [
     VitePWA({
       // you can remove base and scope pwa plugin will use the base on vite: defaults to /
-      base: "/",
-      scope: "/",
-      registerType: "autoUpdate",
+      base: '/',
+      scope: '/',
+      registerType: 'autoUpdate',
       injectRegister: false,
       manifest: false,
       workbox: {
         globDirectory: 'dist',
-        globPatterns: [
-          '**/*.{html,js,css,png,webp,jpg}'
-        ],
+        globPatterns: ['**/*.{html,js,css,png,webp,jpg}'],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/unpkg\.com\/.*/i,
@@ -29,18 +27,18 @@ export default defineConfig({
               cacheName: 'unpkg-libs-cache',
               expiration: {
                 maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+                maxAgeSeconds: 60 * 60 * 24 * 60, // <== 60 days
               },
               cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
       },
       devOptions: {
-        enabled: true
-      }
-    })
-  ]
-})
+        enabled: true,
+      },
+    }),
+  ],
+}));
